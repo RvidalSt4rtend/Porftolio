056b73895b6200ed4789c91ddd3c8c070e09e165
Richard Vidal Gutierrez <rvidal.gutierrez@outlook.com>
2024-08-02 12:14:49 -0500

    update 2 agosto

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			height: {
+				'128': '32rem',  
+				'144': '36rem',  
+				'160': '40rem',
+				'180':'45rem'  
+			  }
+		},
 	},
-	plugins: [],
 }
+
